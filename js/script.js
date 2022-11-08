eacaa71c50db154d86ee0d38a88461ab01d77a9e
pjaskula <pj.jaskula@gmail.com>
2022-11-08 22:44:27 +0100

    Add variables from DOM

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seats:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+const ticketPrice = +movieSelect.value;

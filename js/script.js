f63e2485c09080b601e36c10488d2da0d67ba006
pjaskula <pj.jaskula@gmail.com>
2022-11-08 22:50:17 +0100

    Add click event on seats and toggle selected class

--- js/script.js
+++ js/script.js
@@ -4,3 +4,9 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 const ticketPrice = +movieSelect.value;
+
+container.addEventListener('click', (e) => {
+  if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+    e.target.classList.toggle('selected')
+  }
+})
